127cbcf1f09006d7308dd221534e22c6f60623ce
gary_s <smithgd34@gmail.com>
2022-03-02 11:41:49 -0600

    changed home page

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -9,34 +9,28 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String landingPage() {
-        return "<h1>This is the landing page!</h1>";
-    }
-
-    @GetMapping("/home")
-    public String welcome() {
         return "home";
     }
 
-    @GetMapping("/roll-dice")
-    public String rollDice(){
-//    model.addAttribute("number", number);
-        return "roll-dice";
-    }
-
-    @PostMapping("/roll-dice")
-    public String results(@RequestParam(name = "number") int number, Model model) {
-        int randNum = (int) (Math.random() * 6) + 1;
-        String correct = "You guessed correct with number: " + number;
-        String incorrect = "You guessed incorrect with number: " + number;
-        if (randNum == number) {
-           model.addAttribute("correct", correct) ;
-        }else {
-            model.addAttribute("incorrect", incorrect);
-        }
-        return "roll-dice";
-    }
+//    @GetMapping("/roll-dice")
+//    public String rollDice(){
+////    model.addAttribute("number", number);
+//        return "roll-dice";
+//    }
+//
+//    @PostMapping("/roll-dice")
+//    public String results(@RequestParam(name = "number") int number, Model model) {
+//        int randNum = (int) (Math.random() * 6) + 1;
+//        String correct = "You guessed correct with number: " + number;
+//        String incorrect = "You guessed incorrect with number: " + number;
+//        if (randNum == number) {
+//           model.addAttribute("correct", correct) ;
+//        }else {
+//            model.addAttribute("incorrect", incorrect);
+//        }
+//        return "roll-dice";
+//    }
 
     /// Instructor solve ///
 
